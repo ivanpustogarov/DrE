685f9d5803500b2bdfa33608e770a9dea542701e
Ivan Pustogarov <ip99@cornell.edu>
2017-03-18 19:14:06 -0400

    Improve Makefile and run-dre.sh for toy-example1

--- examples2/toy-example1/run-dre.sh
+++ examples2/toy-example1/run-dre.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-PLUG_PATH=$HOME/Workspaces/DrE
+CHIP=msp430g2553
+
+DRE_ROOT=../../
+DRE=$DRE_ROOT/Release+Asserts/bin/klee
+DRE_OPTS="-search=random-state -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff -plugpath=$DRE_ROOT -chip=$CHIP"
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <file>.bc "
   exit 0
 fi
 
-time ~/Workspaces/DrE/Release+Asserts/bin/klee -search=random-state -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff -plugpath=$PLUG_PATH -chip=msp430g2553 $1
+time $DRE $DRE_OPTS $1
