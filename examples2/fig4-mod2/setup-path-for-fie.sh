e944e5f0813371249317ef112334726ad8503f7d
Ivan Pustogarov <ip99@cornell.edu>
2017-03-19 16:45:27 -0400

    Improve build/compile for examples2
    - fig2, fig4, complex-args, fig4-mod1, fig4-mod2

--- examples2/fig4-mod2/setup-path-for-fie.sh
+++
@@ -1,2 +0,0 @@
-export PATH="/home/ivan/Workspaces/fie/dependencies/clang-2.9/bin/:/home/ivan/bin:/home/ivan/Workspaces/fie/engine/Release+Asserts/bin:/home/ivan/Workspaces/fie/compiler:/home/ivan/Workspaces/llvm-2.9/Release+Asserts/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
-export a="/home/ivan/Workspaces/fie/"
--- examples2/fig4/setup-path-for-fie.sh
+++
@@ -1,2 +0,0 @@
-export PATH="/home/ivan/bin:/home/ivan/Workspaces/fie/engine/Release+Asserts/bin:/home/ivan/Workspaces/fie/compiler:/home/ivan/Workspaces/llvm-2.9/Release+Asserts/bin:$PATH"
-export a="/home/ivan/Workspaces/fie"
