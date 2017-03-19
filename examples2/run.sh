d6ab662f11d182e6333b8371e0da394e02a41f10
Ivan Pustogarov <ip99@cornell.edu>
2017-03-19 15:41:04 -0400

    Improve compile/run-dre for toy-examples

--- examples2/run.sh
+++ examples2/run.sh
@@ -1,19 +1,22 @@
 #! /bin/bash
 
-if [ -z CHIP ]
+# This script is not supposed to be invoked directly,
+# Use scripts inside specific example folders instead,
+# where CIP and DRE_OPTS varaibles are defined.
+
+if [ -z $CHIP ]
 then
-  echo "Go inside a folder an execute 'run-dre.sh'"
+  echo "Go inside a specific example folder an execute 'run-dre.sh'"
   exit 1
 fi
 
 DRE_ROOT=../../
 DRE=$DRE_ROOT/Release+Asserts/bin/klee
-DRE_OPTS="-search=random-state -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff -plugpath=$DRE_ROOT -chip=$CHIP"
-echo "DRE_OPTS=$DRE_OPTS"
+#DRE_OPTS="-search=random-state -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff -plugpath=$DRE_ROOT -chip=$CHIP"
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <file>.bc "
   exit 0
 fi
 
-time $DRE $DRE_OPTS $1
+time $DRE $DRE_OPTS -plugpath=$DRE_ROOT -chip=$CHIP $1
--- examples2/toy-example1/run-dre.sh
+++ examples2/toy-example1/run-dre.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 export CHIP=msp430g2553
+export DRE_OPTS="-search=random-state -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff"
 ../run.sh $1
