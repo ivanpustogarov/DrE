ff4faebc4eb70b2c445c6122636d6f994193805e
Ivan Pustogarov <ip99@cornell.edu>
2017-03-19 17:21:02 -0400

    Improve build/run for examples

--- examples2/pointers-test-2/run-dre.sh
+++ examples2/pointers-test-2/run-dre.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-PLUG_PATH=$HOME/Workspaces/DrE
-
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <file>.bc "
-  exit 0
-fi
-
-time ~/Workspaces/DrE/Release+Asserts/bin/klee -search=bfs -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff -plugpath=$PLUG_PATH -chip=msp430g2553 $1
+export CHIP=msp430g2553
+export DRE_OPTS="-search=bfs -entry-point=main -start-fn=main -target-fn=assert -mmodel=symbolic -imodel=cpuoff"
+../run.sh $1
